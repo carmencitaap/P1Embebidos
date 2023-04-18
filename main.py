86adf22672716ec92537ee27859330db8b8f9335
carmencitaap <cmavendano@miuandes.cl>
2023-04-18 12:39:59 -0400

    on off con boton

--- main.py
+++ main.py
@@ -1,35 +1,24 @@
-# main.py -- put your code here!
-
 import machine
 import time
 
 led_pin = machine.Pin(23, machine.Pin.OUT)
-pin2 = machine.Pin(22,machine.Pin.OUT)
-boton = machine.Pin(17, machine.Pin.IN)
-state = 0
+#pin2 = machine.Pin(22,machine.Pin.OUT)
+boton = machine.Pin(21, machine.Pin.IN)
+led_pin.value(0)
+print("asdjasdkljasl",boton.value())
 while True:
     val = boton.value()
-
+    print("VAL ANTES",val)
+    time.sleep(1)
     if val:
-        if state == 0:
-            state = 1
-        else:
-            state = 0
-    if state == 1:
+        print("VAL DP",val)
         led_pin.value(1)
-        pin2.value(0)
         print("LED ON")
-    else:    state == 0:
+        time.sleep(2)
+    else:
         led_pin.value(0)
-        pin2.value(1)
-    
-        
-    # led_pin.value(1)
-    # pin2.value(1)
-    # print("LED ON")
-    # time.sleep(1)
-    # led_pin.value(0)
-    # pin2.value(0)
-    # print("LED OFF")
-    # time.sleep(1)
-
+        #if not val:
+        #    led_pin.value(0)
+        #    print("LED OFF")
+        #    time.sleep(2)
+    #led_pin.value(0)
