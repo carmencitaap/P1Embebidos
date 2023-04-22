924cf6b365bdd7cf7d2f8a5793b28c0ec370ae21
carmencitaap <cmavendano@miuandes.cl>
2023-04-21 20:00:26 -0400

    SIUUU

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 
 pin_rojo = machine.Pin(23, machine.Pin.OUT)
 pin_verde = machine.Pin(22, machine.Pin.OUT)
-boton = machine.Pin(21, machine.Pin.IN, machine.Pin.PULL_DOWN)
+boton = machine.Pin(21, machine.Pin.IN)
 pin_rojo.value(0)
 pin_verde.value(0)
 
@@ -16,19 +16,20 @@ fruto = 0
 
 while True:
     val = boton.value()
-    print("boton", val)
-    
-    print(frutos_secos[fruto])
-    if not val:
+
+    if val:
+        print(fruto)  
+        print(frutos_secos[fruto])
+        print("boton dpes", val)
         pressed = True
         pin_rojo.value(1)
         print("hola")
-        time.sleep(1)
+        time.sleep(0.1)
         fruto += 1
         if fruto == 3:
             fruto = 0
         boton.value(0)
-    time.sleep(1)
+    time.sleep(0.1)
 
     # if peso <= 20:
     #     pin_rojo.value(1)
