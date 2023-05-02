23f1ff2bf71bac5fd4c43bedf09e1974c8303a21
carmencitaap <cmavendano@miuandes.cl>
2023-05-01 23:23:26 -0400

    angulo 360

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from lcd_api import LcdApi
 from i2c_lcd import I2cLcd
 from stepper import create
 
-def drop_current(fruto,dict):
+def drop_current(fruto,dic):
     if fruto == "Mani":
-        return dict["Mani"][0], dict["Mani"][1]
+        return dic["Mani"][0], dic["Mani"][1]
     elif fruto == "Nueces":
-        return dict["Nueces"][0], dict["Nueces"][1]
+        return dic["Nueces"][0], dic["Nueces"][1]
     elif fruto == "Almendras":
-        return dict["Almendras"][0], dict["Almendras"][1]
+        return dic["Almendras"][0], dic["Almendras"][1]
 
 led_verde = machine.Pin(27, machine.Pin.OUT)
 led_rojo = machine.Pin(23,machine.Pin.OUT)
@@ -31,7 +31,7 @@ led_rojo.value(0)
 
 boton.value(0)
 frutos_secos = ["Mani", "Nueces", "Almendras"]
-caida = {"Mani":[65,3],"Nueces":[65,3],"Almendras":[65,2]}
+caida = {"Mani":[360,1],"Nueces":[65,3],"Almendras":[65,2]}
 fruto = 0
 
 I2C_ADDR = 0x27
@@ -47,7 +47,7 @@ while True:
         lcd.clear()
         current = frutos_secos[fruto]
         lcd.putstr(frutos_secos[fruto])
-      
+        # print(current)
         time.sleep(0.1)
         fruto += 1
         if fruto == 3:
@@ -61,8 +61,8 @@ while True:
             s1 = create(motor1,motor2,motor3,motor4,delay)
             s1.step(1,1)
             s1.angle(angle)
-            s1.step(1,-1)
-            s1.angle(angle,-1)
+            # s1.step(1,-1)
+            # s1.angle(angle,-1)
             boton_drop.value(0)
         except NameError:
             pass    
